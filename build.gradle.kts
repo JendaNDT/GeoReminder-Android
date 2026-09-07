bbf3add672cac2029df48c4ffce450c5de16fe4e
JendaNDT <mcnegr@gmail.com>
2026-09-07 12:53:32 +0200

    docs: poznámka k API 36 buildu

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,4 +1,5 @@
 plugins {
+    // API 36 je oficiálně podporované od AGP 8.10; řada 8.11 vyžaduje Gradle 8.13 + JDK 17.
     id("com.android.application") version "8.11.2" apply false
     id("org.jetbrains.kotlin.android") version "2.0.21" apply false
     id("org.jetbrains.kotlin.plugin.compose") version "2.0.21" apply false
