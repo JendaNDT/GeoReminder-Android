04e158086358537a6eb017795aacdc985dc20378
JendaNDT <mcnegr@gmail.com>
2026-09-07 15:57:20 +0200

    refactor: odstranit globální přepis Locale

--- app/src/main/java/cz/jenda/georeminder/GeoReminderApp.kt
+++ app/src/main/java/cz/jenda/georeminder/GeoReminderApp.kt
@@ -1,6 +1,7 @@
 package cz.jenda.georeminder
 
 import android.app.Application
+import cz.jenda.georeminder.data.FeatureSettings
 import cz.jenda.georeminder.notify.NotificationHelper
 import cz.jenda.georeminder.ui.theme.ThemeController
 
@@ -9,7 +10,6 @@ class GeoReminderApp : Application() {
         super.onCreate()
         NotificationHelper.createChannel(this)
         ThemeController.init(this)
-        cz.jenda.georeminder.data.FeatureSettings.init(this)
-        cz.jenda.georeminder.data.LanguageController.applyLanguage(this, cz.jenda.georeminder.data.FeatureSettings.appLanguage.value)
+        FeatureSettings.init(this)
     }
 }
